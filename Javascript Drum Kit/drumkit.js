567b0b9f2d49b654744c9ba76a476fbc17fc1b30
Sumanbitw <007sumanghosh@gmail.com>
2024-03-14 23:35:49 +0530

    added drumkit functionality

---
+++ Javascript Drum Kit/drumkit.js
@@ -0,0 +1,19 @@
+window.addEventListener("keydown", playSound)
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function removeTransition(e) {
+    console.log(e)
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove("playing")
+}
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("webkitTransitionEnd", removeTransition));
