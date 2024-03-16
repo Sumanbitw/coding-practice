d5818c5818deed3cc20cb1a6f3b76530301ed733
Sumanbitw <007sumanghosh@gmail.com>
2024-03-17 00:51:01 +0530

    implemented async parallel problem

--- promise/asyncParallel.js
+++ promise/asyncParallel.js
@@ -1,30 +1,46 @@
 /**
- * Implement a function that takes a list of async functionsas input and a callback function and executes the 
- * input tasks in parallel i.e all at onceand invokes the callbackafter every task is finished.
+ * Implement a function that takes a list of async functions as input and a callback function and executes the 
+ * input tasks in parallel i.e all at once and invokes the callback after every task is finished.
  */
 
-function asyncParallel(promises, cb) {
-    for(const promise of promises) {
+function executeAsyncTaskInParallel(tasks, cb) {
+    let fulfilled = [];
+    let failed = [];
 
-    }
-}
+    let taskCompletedCount = 0;
 
+    tasks.forEach((task) => {
+        task.then((val) => {
+            fulfilled.push(val);
+        }).catch((error) => {
+            failed.push(error)
+        }).finally(() => {
+            taskCompletedCount++;
 
-function asyncTime(delay){
-    return new Promise((resolve, reject) => {
-        try{
-            setTimeout(() => {
-                resolve(`Completing in ${delay}`)
-            }, delay * 100);
-        }catch(error){
-            reject(error);
-        }
+            if(taskCompletedCount >= tasks.length) {
+                cb(fulfilled, failed);
+            }
+        })
     })
 }
 
-const error = new Error("Oops! Something went wrong");
-const inputArr = [asyncTime(7), async(2), asyncTime(1), asyncTime(6)]
+function createAsyncTask() {
+    const randomVal = Math.floor(Math.random() * 10);
 
-asyncParallel(inputArr, (result) => {
-    console.log(res)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(randomVal < 4) {
+                reject(`Oops! Something went wrong.`);
+            }else {
+                resolve(randomVal * 100);
+            }
+        }, randomVal * 100)
+    })
+}
+
+const inputArr = [createAsyncTask(), createAsyncTask(), createAsyncTask(), createAsyncTask(), createAsyncTask(), createAsyncTask()]
+
+executeAsyncTaskInParallel(inputArr, (success, failed) => {
+    console.error(failed, "failed");
+    console.log(success, "success");
 })
